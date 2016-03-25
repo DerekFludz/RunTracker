dd18a095f40d6863b15cc29e8e4c6e538fa6f4df
Derek Fludzinski <thesource@Dereks-MacBook-Air.local>
2016-03-25 16:00:19 -0400

    create run working

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,6 +1,30 @@
 class RunsController < ApplicationController
 
   def index
+    @user = User.find(params[:user_id])
     @runs = Run.where(user_id: params[:user_id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @run = Run.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @run = Run.new(run_params)
+
+    if @run.save
+      redirect_to user_runs_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def run_params
+    params.require(:run).permit(:run_date, :distance, :pace, :outdoors, :user_id)
+  end
+
 end
