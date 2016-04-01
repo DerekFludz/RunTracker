3df8fd13df03b757821e4a2d72c53bb39bfd8106
Derek Fludzinski <thesource@Dereks-Air.home>
2016-03-31 22:38:37 -0400

    fixing so all stats specific to user

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -3,18 +3,20 @@ class RunsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @runs = Run.where(user_id: params[:user_id])
-    @mile_count = Run.where(user_id: params[:user_id]).sum(:distance)
-    @long_run = Run.maximum(:distance)
-    @ind_runs = Run.where(outdoors: false).count
-    @out_runs = Run.where(outdoors: true).count
-    @long_run_ind = Run.where(outdoors: false).maximum(:distance)
-    @long_run_out = Run.where(outdoors: true).maximum(:distance)
-    @fast_pace = Run.minimum(:pace).to_s.gsub(".",":")
-    @fast_pace_mil = Run.order(:pace).first
-    @fast_pace_ind = Run.where(outdoors: false).minimum(:pace).to_s.gsub(".",":")
-    @fast_pace_out = Run.where(outdoors: true).minimum(:pace).to_s.gsub(".",":")
-    @fast_pace_out_mil = Run.where(outdoors: true).order(:pace).first
-    @fast_pace_ind_mil = Run.where(outdoors: false).order(:pace).first
+    @mile_count = @runs.sum(:distance)
+    @mile_count_ind = @runs.where(outdoors: false).sum(:distance)
+    @mile_count_out = @runs.where(outdoors: true).sum(:distance)
+    @long_run = @runs.maximum(:distance)
+    @ind_runs = @runs.where(outdoors: false).count
+    @out_runs = @runs.where(outdoors: true).count
+    @long_run_ind = @runs.where(outdoors: false).maximum(:distance)
+    @long_run_out = @runs.where(outdoors: true).maximum(:distance)
+    @fast_pace = @runs.minimum(:pace).to_s.gsub(".",":")
+    @fast_pace_mil = @runs.order(:pace).first
+    @fast_pace_ind = @runs.where(outdoors: false).minimum(:pace).to_s.gsub(".",":")
+    @fast_pace_out = @runs.where(outdoors: true).minimum(:pace).to_s.gsub(".",":")
+    @fast_pace_ind_mil = @runs.where(outdoors: false).order(:pace).first
+    @fast_pace_out_mil = @runs.where(outdoors: true).order(:pace).first
   end
 
   def new
